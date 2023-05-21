d69db4bca1cc883e52405714105b449597f17f3c
Danish-chouhan <danishchouhan41@gmail.com>
2023-05-22 00:25:46 +0530

    Q2

--- js/Q2.js
+++ js/Q2.js
@@ -1,1 +1,13 @@
-let Marks = 62;
+let Marks = 51;
+
+if(Marks > 90){
+    console.log("Exelent");
+}else if(Marks >= 80){
+    console.log("Good");
+}else if(Marks >= 70){
+    console.log("Fair");
+}else if(Marks >= 60){
+    console.log("Meet Expactation");
+}else if(Marks < 60){
+    console.log("Below par");
+};
